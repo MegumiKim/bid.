1682017c858fdf0341e71b8bce81bf46f9dace32
Megumi Kimura <kimuramegumi@gmail.com>
2023-02-15 09:49:42 +0100

    hide login btn for already logged in user

--- src/js/tools/checkLogin.mjs
+++ src/js/tools/checkLogin.mjs
@@ -3,8 +3,16 @@ import { clearHTML } from "../utils/clear.mjs";
 export function checkLogin() {
   if (load("accessToken")) {
     const navBtn = document.querySelector("#navBtn");
+    const welcomeBtns = document.querySelector("#welcome-btns");
+
     const avatar = load("userDetails").avatar;
     navBtn.src = avatar;
+
+    welcomeBtns.innerHTML = `<a
+    href="#search-section"
+    type="button"
+    class="btn btn-outline-secondary btn-md px-4 me-sm-2"
+  >Start Auction</a>`;
   } else {
     const loginLink = `<a href="../../../login/" class="me-3" >Login</a>`;
     const navLink = document.querySelector("#nav-link");
