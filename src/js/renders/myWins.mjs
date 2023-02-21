be1c0f4ea0f73a5b8eb90af506703ba06d870ffb
Megumi Kimura <kimuramegumi@gmail.com>
2023-02-21 11:38:56 +0100

    add my wins

---
+++ src/js/renders/myWins.mjs
@@ -0,0 +1,17 @@
+import { fetchSingleEntry } from "../API/listings/singleEntry.mjs";
+import { postListingCard } from "../templates/listingCard.mjs";
+import { userAlert } from "./userAlert.mjs";
+
+export function myWins(data) {
+  const container = document.querySelector("#my-wins-container");
+
+  const wins = data.wins;
+  if (wins.length) {
+    data.wins.forEach(async (win) => {
+      const item = await fetchSingleEntry(win);
+
+      postListingCard(item, container);
+    });
+  }
+  container.innerHTML = "You have not won any item yet.";
+}
