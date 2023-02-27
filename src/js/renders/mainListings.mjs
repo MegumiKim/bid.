0c5dce691468bb18bbc54bea46700ce61ad7116e
Megumi Kimura <kimuramegumi@gmail.com>
2023-02-27 14:36:10 +0100

    change fetchAllListings to direct import

--- src/js/renders/mainListings.mjs
+++ src/js/renders/mainListings.mjs
@@ -1,9 +1,9 @@
-import * as API from "../API/index.mjs";
+import { fetchAllListings } from "../API/index.mjs";
 import * as ui from "../ui/index.mjs";
 import * as listener from "../listeners/index.mjs";
 
 export async function listings() {
-  const allListings = await API.fetchAllListings();
+  const allListings = await fetchAllListings();
 
   const sortSelect = document.querySelector("#sort");
   const sortOption = sortSelect.value;
