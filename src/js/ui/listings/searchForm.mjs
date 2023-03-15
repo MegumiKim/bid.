89addad919c300686295218655aa8c1746495b5b
Megumi Kimura <kimuramegumi@gmail.com>
2023-03-15 11:00:47 +0100

    add evenListener to search-header

--- src/js/ui/listings/searchForm.mjs
+++ src/js/ui/listings/searchForm.mjs
@@ -1,18 +1,21 @@
 import * as listeners from "../../listeners/index.mjs";
 import * as tool from "../../tools/index.mjs";
 export const searchInput = (listings) => {
-  const search = document.querySelector("#search");
+  const searches = document.querySelectorAll(".search");
 
-  search.addEventListener("input", (event) =>
-    tool.debounce(listeners.onSearch(event, listings), 300)
-  );
-  search.addEventListener("keypress", (event) => {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-    }
-  });
+  searches.forEach((search) => {
+    search.addEventListener("input", (event) =>
+      tool.debounce(listeners.onSearch(event, listings), 300)
+    );
+    search.addEventListener("keypress", (event) => {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        window.location.assign("/#search");
+      }
+    });
 
-  search.addEventListener("focusout", (event) => {
-    event.target.value = "";
+    search.addEventListener("focusout", (event) => {
+      event.target.value = "";
+    });
   });
 };
