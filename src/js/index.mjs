3bf5df3a2118867b8b16235d81bd7ed63a75cf4a
Megumi Kimura <kimuramegumi@gmail.com>
2023-10-02 13:05:20 +0200

    Revert "Revert "update router to switch""
    This reverts commit feeac93825647923f647a8f94fc7010c82aa13d9.

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,5 +1,3 @@
 import { routerSwitch } from "./router/index.mjs";
-import { router } from "./router/index.mjs";
 
-router();
-// routerSwitch();
+routerSwitch();
--- src/js/router/index.mjs
+++ src/js/router/index.mjs
@@ -3,42 +3,14 @@ import * as render from "../renders/index.mjs";
 import * as listeners from "../listeners/index.mjs";
 import { checkLogin } from "../tools/checkLogin.mjs";
 
-const path = location.pathname;
-
-export function router() {
-  if (path === "/index.html" || path === "/") {
-    render.listings();
-    ui.myPageBtn();
-    listeners.logout();
-    ui.createListingForm();
-    checkLogin();
-  } else if (path === "/signup/") {
-    ui.signupForm();
-  } else if (path === "/login/") {
-    ui.loginForm();
-  } else if (path === "/product/") {
-    render.singleEntry();
-    listeners.logout();
-    ui.myPageBtn();
-    checkLogin();
-  } else if (path === "/profile/") {
-    render.myPage();
-    listeners.logout();
-    ui.createListingForm();
-    ui.editAvatar();
-    checkLogin();
-  }
-}
-
 export const routerSwitch = () => {
   const path = location.pathname;
-  console.log(path);
   switch (path) {
     case "/signup/":
       ui.signupForm();
       break;
 
-    case "/login":
+    case "/login/":
       ui.loginForm();
       break;
 
@@ -55,6 +27,7 @@ export const routerSwitch = () => {
       ui.createListingForm();
       ui.editAvatar();
       checkLogin();
+      break;
 
     default:
       render.listings();
